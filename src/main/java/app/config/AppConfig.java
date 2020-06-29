da44396212d270fa8160a0698fb17112ec304e99
lis154 <lis154@mail.ru>
2020-06-29 13:58:07 +0300

    commit 2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,7 +1,7 @@
 package app.config;
 
-import app.model.Animal;
 import app.model.Dog;
+import app.model.Timer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +16,13 @@ public class AppConfig {
         Dog dog = new Dog();
         return dog;
     }
+
+    @Bean(name="Timer")
+    public Timer getTimer(){
+        Timer timer = new Timer();
+        return timer;
+    }
+
 }
 
 
